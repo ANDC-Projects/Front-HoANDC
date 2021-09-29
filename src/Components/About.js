3140c1fe7dcc23b9dae5f24aa22a8a6aa8089517
Pratham Sharma <prrrathm@gmail.com>
2021-09-30 00:47:20 +0530

    router changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './components.css';
 
 export const About = () => {
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
 import './components.css';
 import ANDC1 from './media-files/andc1.jpg';
 
