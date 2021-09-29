e35c279833f7f758bb3b3bb66e0d5b57e4c58611
Pratham Sharma <prrrathm@gmail.com>
2021-09-30 00:13:54 +0530

    new fend added

---
+++ src/Components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './components.css';
+
+export const About = () => {
+    return (
+        <div className='text-center '>
+          <h1 className='display-2 ' style={{fontWeight:'bold'}}> About</h1>
+          <p className='lead'></p>
+        </div>
+    )
+}
---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+import './components.css';
+import ANDC1 from './media-files/andc1.jpg';
+
+export const Home = () => {
+    return (
+        <div className='home text-center '>
+          <h1 className='display-1 '> Humans of ANDC.</h1>
+          <p className='lead'>Welcome to the prebuild version of our website.</p>
+        </div>
+    )
+}
